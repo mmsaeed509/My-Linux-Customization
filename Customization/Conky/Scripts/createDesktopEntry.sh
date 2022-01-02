f03aa54536545efc02ae1fa95ec358fe7ec19c90
Eng _ Mahmoud Mohamed <mmsaeed509@gmail.com>
2022-01-02 03:28:32 +0200

    modified createDesktopEntry.sh

--- Customization/Conky/Scripts/createDesktopEntry.sh
+++ Customization/Conky/Scripts/createDesktopEntry.sh
@@ -2,16 +2,20 @@
 
 cd ~/.local/share/applications/
 
-echo [Desktop Entry]>conkyRun.desktop
-echo Comment=>>conkyRun.desktop
-echo Exec=python /home/ozil/Public/conky/conktrun.py>>conkyRun.desktop
-echo Icon=appimagekit-conky-logomark-violet>>conkyRun.desktop
-echo Name=conkyRun>>conkyRun.desktop
-echo NoDisplay=false>>conkyRun.desktop
-echo Path[$e]=>>conkyRun.desktop
-echo StartupNotify=true>>conkyRun.desktop
-echo Terminal=0>>conkyRun.desktop
-echo TerminalOptions=>>conkyRun.desktop
-echo Type=Application>>conkyRun.desktop
-echo X-KDE-SubstituteUID=false>>conkyRun.desktop
-echo X-KDE-Username=>>conkyRun.desktop
+touch conkyRun.desktop
+
+echo '[Desktop Entry]
+Comment=
+Exec=python /home/ozil/Public/conky/conktrun.py
+Icon=appimagekit-conky-logomark-violet
+Name=conkyRun
+NoDisplay=false
+Path[$e]=
+StartupNotify=true
+Terminal=0
+TerminalOptions=
+Type=Application
+X-KDE-SubstituteUID=false
+X-KDE-Username='>conkyRun.desktop
+
+
