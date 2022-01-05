ad8aac65d23146187e605bd85e2ef36af196d94e
Eng _ Mahmoud Mohamed <mmsaeed509@gmail.com>
2022-01-05 23:43:57 +0200

    added conky scripts

---
+++ Customization/Conky/Scripts/createDesktopEntry.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# create conky directory then change directory to conky
+mkdir ~/Public/conky/ && cd ~/Public/conky/
+
+# create script to run conky
+touch conky.sh
+
+# give an executable permission
+sudo chmod +x conky.sh conktrun.py
+
+# adding running script
+echo '#!/bin/bash
+
+conky -c conky.conf'>conky.sh
+
+# creat new desktop entry
+cd ~/.local/share/applications/
+
+touch conkyRun.desktop
+
+echo '[Desktop Entry]
+Comment=
+Exec=python /home/ozil/Public/conky/conktrun.py
+Icon=appimagekit-conky-logomark-violet
+Name=conkyRun
+NoDisplay=false
+Path[$e]=
+StartupNotify=true
+Terminal=0
+TerminalOptions=
+Type=Application
+X-KDE-SubstituteUID=false
+X-KDE-Username='>conkyRun.desktop
+
+
